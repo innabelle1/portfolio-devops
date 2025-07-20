1380bb4e186d540a8058599e52044169efb72ad0
Inna Korn <inna.korn.422@gmail.com>
2025-07-20 17:07:21 +0300

    Fix seed v11

--- seed/seed-pipeline.groovy
+++ seed/seed-pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
   stages {
     stage('Generate Jobs') {
       steps {
-          jobDsl targets: 'seed/create-jenkins-jobs.groovy',
+          jobDsl targets: 'seed/jenkins-jobs-1.groovy',
                  removedJobAction: 'IGNORE',
                  removedViewAction: 'IGNORE',
                  lookupStrategy: 'SEED_JOB'
