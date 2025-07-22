3b45391964f39f047e8f5b0d0fb1651016fe44fd
Inna Korn <inna.korn.422@gmail.com>
2025-07-22 20:03:02 +0300

    fix jenkinsfile v7

--- create-jenkinsfile-services.py
+++ create-jenkinsfile-services.py
@@ -33,8 +33,10 @@ pipeline {{
     stage('Login to ECR') {{
       steps {{
         sh '''
-          aws ecr get-login-password --region $AWS_REGION | \
-          docker login --username AWS --password-stdin $ECR_REGISTRY
+        aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+        aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+        aws configure set default.region us-east-1
+        aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 701173654142.dkr.ecr.us-east-1.amazonaws.com
         '''
       }}
     }}
