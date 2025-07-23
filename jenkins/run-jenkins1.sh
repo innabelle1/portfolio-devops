78f4a62f84e0fb15ba459992369627863c371a6b
Inna Korn <inna.korn.422@gmail.com>
2025-07-23 14:11:10 +0300

    Update jenkins

--- jenkins/run-jenkins1.sh
+++ jenkins/run-jenkins1.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 set -e
 
@@ -7,13 +8,26 @@ docker rm -f jenkins || true
 echo "Build Jenkins..."
 docker build -t jenkins-casc .
 
+# --GID docker.sock
+DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
+
 echo "Run Jenkins  http://localhost:8080"
 docker run -d \
   --name jenkins \
   -p 8080:8080 \
   -v jenkins_home:/var/jenkins_home \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  --group-add $DOCKER_GID \
   jenkins-casc
+
+# check docker & aws in Jenkins container
+echo "Verifying tools inside Jenkins container..."
+
+echo "- Docker CLI version:"
+docker exec -it jenkins docker --version || echo "Docker not found!"
+
+echo "- AWS CLI version:"
+docker exec -it jenkins aws --version || echo "AWS CLI not found!"
 
 sleep 15
 echo "Admin: admin"
