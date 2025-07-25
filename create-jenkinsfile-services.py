cb02990a005270414538cb268565b0d44c4872d0
Inna Korn <inna.korn.422@gmail.com>
2025-07-25 18:43:32 +0300

    Project devops v3

--- create-jenkinsfile-services.py
+++ create-jenkinsfile-services.py
@@ -18,14 +18,13 @@ pipeline {{
 
   parameters {{
     string(name: 'SERVICE_NAME', defaultValue: '{name}', description: 'Microservice name')
-    string(name: 'IMAGE_TAG', defaultValue: '', description: 'Docker image tag (leave empty to use BUILD_NUMBER)')
   }}
 
   environment {{
     AWS_REGION   = 'us-east-1'
     ECR_REGISTRY = '701173654142.dkr.ecr.us-east-1.amazonaws.com'
     ECR_REPO     = "${{ECR_REGISTRY}}/petclinic/${{params.SERVICE_NAME}}"
-    IMAGE_TAG    = "${{params.IMAGE_TAG ?: env.BUILD_NUMBER}}"
+    IMAGE_TAG = "${{GIT_COMMIT}}"
     LOCAL_IMAGE  = "portfolio-devops-${{params.SERVICE_NAME}}:${{IMAGE_TAG}}"
   }}
 
