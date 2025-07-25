c1ba1da164f814e83d9e3b6e55796f2d0fc190c2
Inna Korn <inna.korn.422@gmail.com>
2025-07-25 17:28:45 +0300

    fix

--- run-jobs-jenkins.sh
+++ run-jobs-jenkins.sh
@@ -1,43 +1,31 @@
 #!/bin/bash
 
-#  .env
-if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
-else
-  echo "[ERROR] File .env not found."
-  exit 1
-fi
+JENKINS_URL="http://localhost:8080"
+JENKINS_USER="admin"
+JENKINS_TOKEN="admin123"
+JENKINS_CLI_JAR="./jenkins-cli.jar"
 
-if [[ -z "$JENKINS_URL" || -z "$JENKINS_USER" || -z "$JENKINS_TOKEN" ]]; then
-  echo "[ERROR] JENKINS_URL, JENKINS_USER или JENKINS_TOKEN не заданы в .env"
-  exit 1
-fi
+# list services
+services=(
+  "config-server"
+  "discovery-server"
+  "customers-service"
+  "visits-service"
+  "vets-service"
+  "genai-service"
+  "api-gateway"
+  "admin-server"
+)
 
-JENKINS_CLI="jenkins-cli.jar"
+# run jobs
+for service in "${services[@]}"; do
+  echo "run jobs for: petclinic-${service}"
 
-# check jenkins-cli.jar
-if [ ! -f "$JENKINS_CLI" ]; then
-  echo "[INFO] dowload Jenkins CLI..."
-  curl -sSL "$JENKINS_URL/jnlpJars/jenkins-cli.jar" -o "$JENKINS_CLI"
-fi
-
-# list of jobs petclinic-*
-echo "[INFO] List of  Jenkins jobs 'petclinic-*'..."
-jobs=$(java -jar "$JENKINS_CLI" -s "$JENKINS_URL" -auth "$JENKINS_USER:$JENKINS_TOKEN" list-jobs | grep '^petclinic-')
-
-if [[ -z "$jobs" ]]; then
-  echo "[WARN] No job with 'petclinic-'"
-  exit 1
-fi
-
-echo "[INFO] Found $(echo "$jobs" | wc -l) jobs:"
-echo "$jobs"
-echo
-
-# run all jobs
-for job in $jobs; do
-  echo "[RUNNING] $job ..."
-  java -jar "$JENKINS_CLI" -s "$JENKINS_URL" -auth "$JENKINS_USER:$JENKINS_TOKEN" build "$job"
+  java -jar "$JENKINS_CLI_JAR" -s "$JENKINS_URL" -auth "$JENKINS_USER:$JENKINS_TOKEN" \
+    build "petclinic-${service}" \
+    -p "SERVICE_NAME=${service}" \
+    -p "IMAGE_TAG=latest" \
+    -w || echo "Failure: petclinic-${service}"
 done
 
-echo "[DONE] All jobs running."
+echo "all pipelines in  Jenkins"
